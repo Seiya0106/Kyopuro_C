62d82ad2fed3a6b152eecfe7efa9e113d71640a6
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-08-17 12:25:09 +0900

    Update Beginner364.py

--- Beginner364.py
+++ Beginner364.py
@@ -1,25 +1,22 @@
 N, X, Y = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
+A.sort(reverse=True)
+B.sort(reverse=True)
 ans_a = 0
 ans_b = 0
 food_a = 0
 food_b = 0
 
-for _ in range(N):
-  topa = max(A)
-  food_a += topa
-  A.remove(topa)
-  ans_a += 1
-  if food_a > X:
-    break
-  
-for _ in range(N):
-  topb = max(B)
-  food_b += topb
-  B.remove(topb)
-  ans_b += 1
-  if food_b > Y:
-    break
+for a in A:
+    food_a += a
+    ans_a += 1
+    if food_a > X:
+        break
+for b in B:
+    food_b += b
+    ans_b += 1
+    if food_b > Y:
+        break
 
 print(min(ans_a, ans_b))
