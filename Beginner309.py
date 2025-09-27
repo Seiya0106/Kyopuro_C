a840980164aa76ffd86194197dd916bdfec73acc
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-09-27 19:58:59 +0900

    Update Beginner309.py

--- Beginner309.py
+++ Beginner309.py
@@ -1,5 +1,4 @@
 N, K = map(int, input().split())
-day = 1
 total = 0
 # N行分のペアを受け取る
 pairs = []
@@ -7,6 +6,7 @@ for _ in range(N):
     a, b = map(int, input().split())
     pairs.append((a, b))
 pairs.sort(reverse=True)
+
 for nums in pairs:
     total += nums[1]
     if total > K:
