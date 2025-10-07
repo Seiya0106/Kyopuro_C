b7683a34b853ff58d9fafe7b01834194760ec991
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-10-07 12:19:36 +0900

    Create Beginner085.py

---
+++ Beginner085.py
@@ -0,0 +1,13 @@
+# ヒントあり
+N, Y = map(int, input().split())
+
+for x in range(N + 1):
+    for y in range(N + 1):
+        z = N - x - y
+        if z < 0:
+            continue
+        if 10000 * x + 5000 * y + 1000 * z == Y:
+            print(x, y, z)
+            exit()
+
+print(-1, -1, -1)
