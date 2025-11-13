ad6088c09c4ea616653f531fa841a71db503cbbf
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-11-13 17:50:14 +0900

    Create Beginner398.py

---
+++ Beginner398.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+
+N = int(input())
+A = list(map(int, input().split()))
+
+d = defaultdict(list)
+for i, a in enumerate(A):
+    d[a].append(i)
+
+for key in sorted(d, reverse=True):
+    if len(d[key]) == 1:
+        print(d[key][0] + 1)
+        exit()
+print(-1)
