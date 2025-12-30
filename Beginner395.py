df185bc2c67e0c95f114acc92ca9a95cc8509832
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2025-12-30 10:28:38 +0900

    Update Beginner395.py

--- Beginner395.py
+++ Beginner395.py
@@ -1,21 +1,19 @@
-from collections import Counter
-
 n = int(input())
-a = list(map(int, input().split()))
-nums = Counter(a)
-# 全て1の場合は条件を満たさない
-if all(n == 1 for n in nums.values()):
-    print(-1)
-    exit()
+A = list(map(int, input().split()))
+
+pairs = []
+for i, a in enumerate(A):
+    pairs.append((a, i))
+pairs.sort()
+
+ans = n
+ok = False
+for i in range(n-1):
+    if pairs[i][0] == pairs[i+1][0]:
+        ok = True
+        ans = min(ans, pairs[i+1][1] - pairs[i][1] + 1)
+
+if ok:
+    print(ans)
 else:
-    com = []
-    length = 2 * (10 ** 5)
-    # 数字とインデックスを保持
-    for i, num in enumerate(a):
-        com.append((num, i))
-    com.sort()
-    for j in range(n - 1):
-        # 同じ数字かどうか判定し、距離を計算
-        if com[j][0] == com[j + 1][0]:
-            length = min(length, com[j+1][1] - com[j][1] + 1)
-print(length)
+    print(-1)
