1589c196e7e9783ccfd4772b0b8f35dbbff6fc94
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2026-02-21 10:09:08 +0900

    Create Beginner306.py

---
+++ Beginner306.py
@@ -0,0 +1,10 @@
+n = int(input())
+A = list(map(int,input().split()))
+cnt = [0 for _ in range(3*n)]
+ans = []
+for i in A:
+    cnt[i] += 1
+    if cnt[i] == 2:
+        ans.append(i)
+
+print(*ans)
