7d9f985535b167204ab2d67722018a1f8e9b7613
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2026-02-26 15:52:36 +0900

    Update Beginner292.py

--- Beginner292.py
+++ Beginner292.py
@@ -1,33 +1,28 @@
-import sys
+n = int(input())
+ans = 0
 
-def solve():
-    # 標準入力からの読み込み
-    input = sys.stdin.read
-    data = input().split()
-    if not data:
-        return
-    
-    N = int(data[0])
-    
-    # 約数の個数を前計算するための配列
-    # d[i] は整数 i の約数の個数
-    d = [0] * (N + 1)
-    
-    # 1 から N までの数について約数の個数をカウント
-    # 調和級数の和により計算量は O(N log N)
-    for i in range(1, N + 1):
-        for j in range(i, N + 1, i):
-            d[j] += 1
-            
-    ans = 0
-    # AB = x, CD = N - x となる x を全探索
-    # 1 <= x <= N - 1
-    for x in range(1, N):
-        y = N - x
-        # (A,B)の組み合わせ数 * (C,D)の組み合わせ数
-        ans += d[x] * d[y]
-        
-    print(ans)
+for i in range(1, n):
+    x = i
+    y = n - i
 
-if __name__ == '__main__':
-    solve()
+    dx = 0
+    j = 1
+    while j * j <= x:
+        if x % j == 0:
+            dx += 1
+            if x != j * j:
+                dx += 1
+        j += 1
+
+    dy = 0
+    j = 1
+    while j * j <= y:
+        if y % j == 0:
+            dy += 1
+            if y != j * j:
+                dy += 1
+        j += 1
+
+    ans += dx * dy
+
+print(ans)
